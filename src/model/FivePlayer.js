222404703175d70fdb1d3295bf1ce2ef174f8582
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-07-20 22:34:19 +0200

    :art: Fix doc indentation

--- src/model/FivePlayer.js
+++ src/model/FivePlayer.js
@@ -6,45 +6,45 @@
  */
 class FivePlayer {
   /** *
-     * Constructor for class
-     * @param {Object} payload Data from API
-     */
+   * Constructor for class
+   * @param {Object} payload Data from API
+   */
   constructor(payload) {
     const {endpoint, id, identifiers, name, ping} = payload;
 
     /** *
-         * endpoint represents the player's ip address from the server
-         * @type {string}
-         * @public
-         */
+     * endpoint represents the player's ip address from the server
+     * @type {string}
+     * @public
+     */
     this.endpoint = endpoint;
 
     /** *
-         * id represents the player's id from the server
-         * @type {number}
-         * @public
-         */
+     * id represents the player's id from the server
+     * @type {number}
+     * @public
+     */
     this.id = id;
 
     /** *
-         * identifiers represents all the ways the player can be identified
-         * @type {Array<string>}
-         * @public
-         */
+     * identifiers represents all the ways the player can be identified
+     * @type {Array<string>}
+     * @public
+     */
     this.identifiers = identifiers;
 
     /** *
-         * name represents the player's name from the server
-         * @type {string}
-         * @public
-         */
+     * name represents the player's name from the server
+     * @type {string}
+     * @public
+     */
     this.name = name;
 
     /** *
-         * ping represents the player's ping from the server
-         * @type {number}
-         * @public
-         */
+     * ping represents the player's ping from the server
+     * @type {number}
+     * @public
+     */
     this.ping = ping;
   }
 }
