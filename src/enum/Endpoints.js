987eb77ef46d5672fa9a4790cd0cca09cf485b1a
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-07-21 03:03:49 +0200

    :hammer: Add Endpoints enum

---
+++ src/enum/Endpoints.js
@@ -0,0 +1,7 @@
+class Endpoints {
+  static CFX_MASTERLIST = 'https://servers-frontend.fivem.net/api/servers/single/';
+  static CFX_STATUS_SUMMARY = 'https://status.cfx.re/api/v2/status.json';
+  static CFX_STATUS_COMPONENTS = 'https://status.cfx.re/api/v2/components.json';
+}
+
+module.exports = Endpoints;
